d0802592a47289a1848915ce6f4dd1442495dd41
fedebram <163430291+fedebram@users.noreply.github.com>
2024-05-02 18:41:01 +0200

    Setting webpack developer mode, source-map, webpack-dev server (not really helpful)

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,20 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
+  devServer: {
+    watchFiles: ['./src/index.html'],
+  },
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
